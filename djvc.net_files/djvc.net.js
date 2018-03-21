26f74d70d3835b4b9563446f2324d60257c3ace9
Jack <teknologyguru@gmail.com>
2018-03-21 16:43:29 -0500

    Initial commit of djvc.net assets.

---
+++ djvc.net_files/djvc.net.js
@@ -0,0 +1,7 @@
+// Created by iWeb 3.0.4 local-build-20121206
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('djvc.net_files/djvc.netMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');performPostEffectsFixups()}
